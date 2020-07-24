789a827fbd09928bbede59f00d3ca56d09f26574
Kevin Abrar Khansa <kevariable@gmail.com>
2020-07-24 14:34:43 +0700

    Export

---
+++ app/Exports/UsersExport.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Exports;
+
+use App\User;
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+// use Maatwebsite\Excel\Concerns\FromCollection;
+
+class UsersExport implements FromCollection
+{
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public function collection()
+    {
+        return User::select('id', 'name')->where('id', '>', 25)->get();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Exports\UsersExport;
+use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users/export', 'UserController');
