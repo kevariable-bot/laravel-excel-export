782ae1dde1bef066531605e629549c05cc7be7e2
Kevin Abrar Khansa <kevariable@gmail.com>
2020-07-25 12:27:36 +0700

    Event

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -4,15 +4,9 @@ namespace App\Exports;
 
 use App\User;
 use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromQuery;
-use Maatwebsite\Excel\Concerns\WithColumnFormatting;
-use Maatwebsite\Excel\Concerns\WithHeadings;
-use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
-use PhpOffice\PhpSpreadsheet\Shared\Date;
-use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class UsersExport implements FromQuery, WithMapping, WithHeadings, WithColumnFormatting, WithMultipleSheets
+class UsersExport implements WithMultipleSheets
 {
     use Exportable;
 
@@ -25,37 +19,5 @@ class UsersExport implements FromQuery, WithMapping, WithHeadings, WithColumnFor
         }
 
         return $sheets;
-    }
-
-    public function query()
-    {
-        return User::where('id', '>', 25);
-    }
-
-    public function map($user): array
-    {
-        return [
-            'ini id ke - ' . $user->id,
-            $user->name,
-            $user->email,
-            Date::dateTimeToExcel($user->created_at)
-        ];
-    }
-
-    public function headings(): array
-    {
-        return [
-            '#',
-            'Name',
-            'Email',
-            'Time'
-        ];
-    }
-
-    public function columnFormats(): array
-    {
-        return [
-            'D' => NumberFormat::FORMAT_DATE_DDMMYYYY
-        ];
     }
 }
--- app/Exports/UsersPerMonthSheet.php
+++ app/Exports/UsersPerMonthSheet.php
@@ -4,9 +4,13 @@ namespace App\Exports;
 
 use App\User;
 use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithEvents; // import dan gunakan di implement
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Events\BeforeSheet;
 
-class UsersPerMonthSheet implements FromQuery, WithTitle
+class UsersPerMonthSheet implements FromQuery, WithTitle, WithHeadings, WithEvents
 {
     /**
      * @return Builder
@@ -14,8 +18,7 @@ class UsersPerMonthSheet implements FromQuery, WithTitle
     public function query()
     {
         return User
-            ::query()
-            ->where('id', '>', 40);
+            ::query();
     }
 
     /**
@@ -23,6 +26,34 @@ class UsersPerMonthSheet implements FromQuery, WithTitle
      */
     public function title(): string
     {
-        return 'Month ';
+        return 'Month '; // nama sheet yang sebelumnya digunakan pada WithMultipleSheet.
+    }
+
+    public function headings(): array
+    {
+        // heading yang akan digunakan nantinya untuk dibuat bold
+        return [
+            'id',
+            'name',
+            'point',
+            'email',
+            'email verification',
+            'update_at',
+            'created_at'
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            BeforeSheet::class => function (BeforeSheet $event) {
+                $event->sheet->setCellValue('A1', 'Hello');
+            },
+
+            AfterSheet::class => function (AfterSheet $event) {
+                $event->sheet->insertNewRowBefore(/* line ke 7 */7, /* row yang mau ditambah */ 2);
+                $event->sheet->insertNewColumnBefore('A', /* row yang mau ditambah */ 2);
+            }
+        ];
     }
 }
